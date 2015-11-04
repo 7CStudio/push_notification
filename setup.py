fc10a77b19f2caf15815d7dc6e35a02ec03b16a9
RaviShanker B <ravishanker414@gmail.com>
2015-11-04 17:30:38 +0530

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='push_notification',
-    version='0.0.3',
+    version='0.0.4',
     packages=['push_notification'],
     license='MIT',
     install_requires=[
